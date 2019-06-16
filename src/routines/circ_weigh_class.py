d0fe53b38ebc0ff76ea8e203a166c28e24b1cccc
r.hawke <rebecca.hawke@measurement.govt.nz>
2019-06-17 11:21:58 +1200

    CircWeigh class now has methods to analyse the circular weighing data including to determine the best order of drift correction.

--- src/routines/circ_weigh_class.py
+++ src/routines/circ_weigh_class.py
@@ -6,6 +6,9 @@ For more information, see 'A General Approach to Comparisons in the Presence of 
 
 
 class CircWeigh(object):
+    _sequences = {2: 5, 3: 4, 4: 3, 5: 3}  # key: number of weight groups in weighing, value: number of cycles
+    _driftorder = {'no drift': 0, 'linear drift': 1, 'quadratic drift': 2, 'cubic drift': 3}
+
     def __init__(self, scheme_entry):
         """Initialises a circular weighing for a single weighing in the scheme
 
@@ -25,10 +28,16 @@ class CircWeigh(object):
             scheme_entry4 = "1 1s 0.5+0.5s"
 
         """
-        self._sequences = {2: 5, 3: 4, 4: 3, 5: 3}  # key: number of weight groups in weighing, value: number of cycles
         self.wtgrps = scheme_entry.split()
-        self.num_wtgrps = len(self.wtgrps)
+        self.num_wtgrps = len(self.wtgrps)  # q in paper
         self.num_cycles = (self._sequences[self.num_wtgrps])
+        self.num_readings = self.num_cycles*self.num_wtgrps  # p in paper
+        self.matrices = {}
+        self.t_matrices = {}
+        self.b = {}
+        self.residuals = {}
+        self.stdev = {}
+        self.varcovar = {}
 
     def generate_design_matrices(self, times=[]):
         """Sets up design matrices for linear, quadratic and cubic drift
@@ -36,7 +45,7 @@ class CircWeigh(object):
         Parameters
         ----------
         times : list
-            list of times for each measurement.  Ideally these will be equally spaced.
+            list of times for each measurement.  Ideally these will be equally spaced in time.
             Could be [0, 1, 2, 3, ...] or could be from 'Timestamps' attr of dataset
 
         Returns
@@ -48,9 +57,8 @@ class CircWeigh(object):
         M3 : numpy array
             design matrix for cubic drift
         """
-        num_readings = self.num_cycles*self.num_wtgrps
         if times == [] or not all(times):  # Fill time as simple ascending array, [0,1,2,3...]
-            times = np.arange(num_readings)
+            times = np.arange(self.num_readings)
         else:  # Ensure that time is a numpy array object.
             times = np.array(times)
 
@@ -64,10 +72,57 @@ class CircWeigh(object):
         dm2_T = np.vstack((dm1_T, times**2))
         dm3_T = np.vstack((dm2_T, times**3))
 
-        self.dm0 = dm0_T.T
-        self.dm1 = dm1_T.T
-        self.dm2 = dm2_T.T
-        self.dm3 = dm3_T.T
+        self.t_matrices = {'no drift': dm0_T, 'linear drift': dm1_T, 'quadratic drift': dm2_T, 'cubic drift': dm3_T}
+        self.matrices = {'no drift': dm0_T.T, 'linear drift': dm1_T.T, 'quadratic drift': dm2_T.T, 'cubic drift': dm3_T.T}
+
+        return self.t_matrices, self.matrices
+
+    def determine_drift(self, dataset):
+        """This method takes a dataset from a weighing and calculates expected values, residuals, standard deviation,
+         and variance-covariance matrices for each of the drift correction options.
+         All matrices are stored as instance variables.
+
+        Parameters
+        ----------
+        dataset : array
+            array from h5py dataset object e.g. weighing[:,:]
+
+        Returns
+        -------
+        h : str
+            Order of drift correction which gives the smallest standard deviation
+
+        """
+        # convert data array to 1D list
+        self.y_col = np.reshape(dataset, self.num_readings)
+
+        # calculate vector of expected values
+        for drift, xT in self.t_matrices.items():
+            self.xTx_inv = inv(np.dot(xT, self.matrices[drift]))
+            # print('xTx_inv = ')
+            # print(self.xTx_inv)
+            self.b[drift] = multi_dot([self.xTx_inv, xT, self.y_col])
+            # print('b = ')
+            # print(self.b)
+
+            # calculate the residuals, variance and variance-covariance matrix:
+            self.residuals[drift] = self.y_col - np.dot(self.matrices[drift], self.b[drift])
+            print('residuals = ')
+            print(self.residuals[drift])
+
+            var = np.dot(self.residuals[drift].T, self.residuals[drift]) / (self.num_readings - self.num_wtgrps - self._driftorder[drift])
+            print('variance, \u03C3\u00b2 = ',var.item(0))
+            self.stdev[drift] = np.sqrt(var.item(0))
+            print('standard deviation, \u03C3 = ', self.stdev[drift])
+
+            self.varcovar[drift] = np.multiply(var, self.xTx_inv)
+            print('variance-covariance matrix, C = ')
+            print(self.varcovar[drift])
+            print('for', str(self.num_wtgrps),'item(s), and', drift, 'correction')
+
+        return min(self.stdev, key=self.stdev.get)
+
+''''''
 
 
 
