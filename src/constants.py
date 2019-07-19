5744bd3c1ae3bab1eafbb502e5bcd9a4b8d78e22
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2019-07-19 13:56:49 +1200

    constants: A repository for constants and symbols

---
+++ src/constants.py
@@ -0,0 +1,13 @@
+# A repository for constants and symbols used in the mass weighing program
+
+MU_STR = 'µ'
+SIGMA_STR = ''
+
+DEGREE_SIGN = '°'
+IN_DEGREES_C = ' ('+DEGREE_SIGN+'C)'
+
+MIN_T = 18.1
+MAX_T = 21.9
+
+MIN_RH = 33
+MAX_RH = 67
