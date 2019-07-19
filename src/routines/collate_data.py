ef28984f58d457b12db3130c7a2bbeb2e13b91f3
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2019-07-19 17:37:55 +1200

    collate_data: includes function to average data from automatic weighing
    returns collated data for final_mass_calc and also bool for if acceptance criteria have been met overall.  Allows exclusion of particular datasets as needed, by using 'Exclude?' key with True bool.

--- src/routines/collate_data.py
+++ src/routines/collate_data.py
@@ -2,12 +2,91 @@
 
 import os
 from msl.io import read
-from ..constants import MU_STR, SUFFIX
+from ..constants import MU_STR, SUFFIX, SQRT_F
+from ..log import log
 import numpy as np
 
 
-def collate_data_from_json(folder, filename, scheme_entry):
+def collate_a_data_from_json(folder, filename, scheme_entry):
+    """Use this function for an automatic weighing where individual weighings are not likely to meet SQRT_F criterion,
+    but the ensemble average is.
+
+    Parameters
+    ----------
+    folder
+    filename
+    scheme_entry
+
+    The json file must have analysis datasets with fields and formats as follows:
+    dtype = [('+ weight group', 'O'), ('- weight group', 'O'), ('mass difference', '<f8'), ...
+
+    Returns
+    -------
+    tuple of
+    [0] structured array of averaged weighing data in grams, if meets acceptance criteria, else None
+    [1] bool indicating whether average data meets acceptance criteria for weighing
+
     """
+    url = folder + "\\" + filename + '.json'
+    if not os.path.isfile(url):
+        raise IOError('File does not exist {!r}'.format(url))
+
+    root = read(url)
+    wt_grps = scheme_entry.split()
+    num_wt_grps = len(wt_grps)
+    collated = {'Stdev': [], 'Max stdev': []}
+    for grp in wt_grps:
+        collated[grp] = []
+
+    for dataset in root.datasets():
+        dname = dataset.name.split('_')  # split('/')[-1].
+        exclude = dataset.metadata.get('Exclude?')
+
+        if dname[0][-8:] == 'analysis' and not exclude:
+            run_id = 'run_' + dname[2]
+
+            meta = root.require_dataset(root['Circular Weighings'][scheme_entry].name + '/measurement_' + run_id)
+            collated['Stdev'].append(meta.metadata.get('Stdev for balance (' + MU_STR + 'g)'))
+            collated['Max stdev'].append(meta.metadata.get('Max stdev from CircWeigh ('+MU_STR+'g)'))
+
+            bal_unit = dataset.metadata.get('Mass unit')
+            for i in range(dataset.shape[0]):
+                key = dataset['+ weight group'][i]
+                collated[key].append(dataset['mass difference'][i]*SUFFIX[bal_unit])
+
+    for key, value in collated.items():
+        collated[key] = (np.average(value), np.std(value))
+
+    inputdata = np.empty(num_wt_grps-1,
+                         dtype=[('+ weight group', object), ('- weight group', object),
+                                ('mass difference (g)', 'float64'),
+                                ('balance uncertainty (' + MU_STR + 'g)', 'float64')])
+
+    acceptable = True
+    massdiff = np.empty(num_wt_grps)
+    stdevs = np.empty(num_wt_grps)
+    for i, grp in enumerate(wt_grps):
+        massdiff[i] = collated[grp][0]
+        stdevs[i] = collated[grp][1]
+        if not collated[grp][1] < SQRT_F*collated['Max stdev'][0]*SUFFIX['ug']:
+            log.warning('Stdev of differences for + weight group ' + grp + ' falls outside acceptable limits')
+            acceptable = False
+
+    if not (np.round(np.sum(massdiff), 12) == 0):
+        log.warning('Mass differences for ' + scheme_entry + ' do not sum to zero within reasonable limits')
+        acceptable = False
+
+    inputdata[:]['+ weight group'] = wt_grps[:-1]
+    inputdata[:]['- weight group'] = np.roll(wt_grps, -1)[:-1]
+    inputdata[:]['mass difference (g)'] = massdiff[:-1]
+    for row in range(num_wt_grps - 1):
+        inputdata[row:]['balance uncertainty ('+MU_STR+'g)'] = collated['Stdev'][0]
+
+    return inputdata, acceptable
+
+
+def collate_m_data_from_json(folder, filename, scheme_entry):
+    """Use this function to collate one or two runs from an mde or mw weighing
 
     Parameters
     ----------
@@ -39,8 +118,9 @@ def collate_data_from_json(folder, filename, scheme_entry):
 
     for dataset in root.datasets():
         dname = dataset.name.split('_')  # split('/')[-1].
+
         ok = dataset.metadata.get('Acceptance met?')
-        if dname[0][-8:] == 'analysis': # and ok: TODO: add back in when sorted
+        if dname[0][-8:] == 'analysis' and ok:
             run_id = 'run_' + dname[2]
 
             meta = root.require_dataset(root['Circular Weighings'][scheme_entry].name + '/measurement_' + run_id)
