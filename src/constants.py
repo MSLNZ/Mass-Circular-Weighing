34e30ce0cd0c99e8540df301025ed02a761a4a89
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2019-07-19 17:44:10 +1200

    constants: added EXCL factor
    currently very arbitrary - could do more research into what constitutes an outlier or bogus weighing.

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,7 @@
 # A repository for constants and symbols used in the mass weighing program
 
-MU_STR = 'µ'
-SIGMA_STR = 'σ'
+MU_STR = 'u'                # ALT+0181 or 'µ'
+SIGMA_STR = 'σ'             # \u03C3 for sigma sign
 SQUARED_STR = '²'
 
 SUFFIX = {'µg': 1e-6, 'ug': 1e-6, 'mg': 1e-3, 'g': 1, 'kg': 1e3}
@@ -17,4 +17,8 @@ MAX_RH = 67
 
 MAX_T_CHANGE = 0.5          # max allowed change in T IN_DEGREES_C over course of weighing
 MAX_RH_CHANGE = 15          # max allowed change in RH in % over course of weighing
+
+SQRT_F = 1.4                # criterion for accepting single weighing analysis
+EXCL = 3                    # criterion for excluding a single weighing within an automatic weighing sequence.
+                            # note that this constant is currently set to a rather arbitrary value...
 
