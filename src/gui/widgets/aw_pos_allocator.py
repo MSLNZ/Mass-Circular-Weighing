283c7e2ac8ba6bdc46c63529226af9fd4f6b2a09
r.hawke <rebecca.hawke@measurement.govt.nz>
2020-03-30 10:58:55 +1300

    AW_Bal allocator as QWidgets.QDialog

--- src/gui/widgets/aw_pos_allocator.py
+++ src/gui/widgets/aw_pos_allocator.py
@@ -1,48 +1,24 @@
-from msl.qt import QtCore, QtWidgets, Button, Signal, Thread, Worker
+from msl.qt import QtWidgets, Button
 import numpy as np
 
 
-class AllocatorWorker(Worker):
+class AllocatorDialog(QtWidgets.QDialog):
 
-    def __init__(self, parent, *args, **kwargs):
-        super(AllocatorWorker, self).__init__()
-        self.parent = parent
-        self.args = args
-        self.kwargs = kwargs
+    def __init__(self, num_pos, wtgrps, parent=None):
+        super().__init__(parent=parent)
+        self.setWindowTitle('Position Allocator')
 
-    def process(self):
-        self.parent.signal_alloc.emit(self.args)
-
-
-class AllocatorThread(Thread):
-    signal_alloc = Signal(list)
-    signal_alloc_done = Signal()
-
-    def __init__(self, num_pos, wtgrps):
-        super(AllocatorThread, self).__init__(AllocatorWorker)
-
-        self.num_pos = num_pos
         self.wtgrps = wtgrps
         self.positions = []
 
-        self.window = QtWidgets.QWidget()
-        self.window.setWindowTitle('Position Allocator')
         self.pos_list = QtWidgets.QListWidget()
+        self.pos_list.addItems([f'Position {i+1}' for i in range(num_pos)])
+
         self.wtgrp_list = QtWidgets.QListWidget()
-        self.make_allocator_window()
-
-        self.signal_alloc.connect(self.accept_loading_order)
-
-    def make_allocator_window(self):
-        pos = []
-        for i in range(self.num_pos):
-            pos.append('Position '+str(i+1))
-        self.pos_list.addItems(pos)
-
         self.wtgrp_list.addItems(self.wtgrps)
-        while len(self.wtgrp_list) < self.num_pos:
+        while self.wtgrp_list.count() < num_pos:
             self.wtgrp_list.addItem('empty')
-        self.wtgrp_list.setDragDropMode(self.wtgrp_list.InternalMove)
+        self.wtgrp_list.setDragDropMode(QtWidgets.QListWidget.InternalMove)
 
         lists = QtWidgets.QWidget()
         hbox = QtWidgets.QHBoxLayout()
@@ -57,15 +33,10 @@ class AllocatorThread(Thread):
         vbox.addWidget(lists)
         vbox.addWidget(shuffle)
         vbox.addWidget(accept)
-        self.window.setLayout(vbox)
-
-        self.signal_alloc_done.connect(self.window.close)
+        self.setLayout(vbox)
 
     def get_all_items(self):
-        loading = []
-        for i in range(self.num_pos):
-            loading.append(self.wtgrp_list.item(i).text())
-        return loading
+        return [self.wtgrp_list.item(i).text() for i in range(self.wtgrp_list.count())]
 
     def roll(self):
         loading = self.get_all_items()
@@ -75,41 +46,6 @@ class AllocatorThread(Thread):
     def accept_loading_order(self):
         loading = self.get_all_items()
         for grp in self.wtgrps:
-            print(grp, 'is in position', str(loading.index(grp)+1))
             self.positions.append(loading.index(grp)+1)
-
-        if QtWidgets.QApplication.instance() is not None:
-            self.signal_alloc_done.emit()
-
-    def wait_for_reply(self):
-        """Block loop until the popup window is closed"""
-        if QtWidgets.QApplication.instance() is not None:
-            loop = QtCore.QEventLoop()
-            self.signal_alloc_done.connect(loop.quit)
-            loop.exec_()
-        return self.positions
-
-    def show(self, *args, **kwargs):
-        if QtWidgets.QApplication.instance() is not None:  # this is opposite to what you have?
-            self.window.show()
-        else:
-            self.start(self, *args, **kwargs)
-
-
-if __name__ == '__main__':
-    import sys
-    from msl.qt import application, excepthook
-
-    sys.excepthook = excepthook
-
-    gui = application()
-
-    num_pos = 5
-    se = 'A B C'
-    wtgrps = se.split()
-
-    w = AllocatorThread(num_pos, wtgrps)
-    w.show()
-    print(w.wait_for_reply())
-    gui.exec()
+        self.close()
 
