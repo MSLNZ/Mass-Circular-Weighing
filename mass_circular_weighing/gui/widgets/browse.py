8e9198f79408b367bc83863253db90000b4107d6
r.hawke <rebecca.hawke@measurement.govt.nz>
2020-04-24 18:18:43 +1200

    Browse changed to include either file or folder select (using keyword find)
    find='folder' or find='file'.  Hardcoded links changed to refer to values in constants.py at Blair's prompting (thanks Blair!).

--- mass_circular_weighing/gui/widgets/browse.py
+++ mass_circular_weighing/gui/widgets/browse.py
@@ -1,5 +1,7 @@
 from msl.qt import QtWidgets, Button, prompt
 
+from ...constants import sample_data_folder, mass_folder, H_drive
+
 
 def label(name):
     lbl = QtWidgets.QLabel(name)
@@ -9,15 +11,29 @@ def label(name):
 
 class Browse(QtWidgets.QWidget):
 
-    def __init__(self, default, icon):
+    def __init__(self, default, icon, find='folder'):
+        """A browse widget which combines a textbox linked with a file or folder browse pop-up.
+        Default is a browser that looks for a folder.
+
+        Parameters
+        ----------
+        default
+        icon
+        find : str
+            this argument specifies which type of browse is created: file or folder.
+        """
         super(Browse, self).__init__()
 
         self.textbox = QtWidgets.QLineEdit(default)
-        self.button = Button(icon=icon, left_click=self.display_folder)
+        if find == 'folder':
+            self.button = Button(icon=icon, left_click=self.display_folder)
+        elif find == 'file':
+            self.button = Button(icon=icon, left_click=self.display_file)
+        else:
+            self.button = Button(icon=icon)
         self.button.add_menu_item(text='Mass drive', triggered=self.mass_drive_selected)
         self.button.add_menu_item(text='H: drive', triggered=self.hdrive_selected)
         self.button.add_menu_item(text='Sample data', triggered=self.sampledata_selected)
-        #self.button.add_menu_item('open browser', shortcut='CTRL+O')
 
         hbox = QtWidgets.QHBoxLayout()
         hbox.addWidget(self.textbox)
@@ -28,42 +44,15 @@ class Browse(QtWidgets.QWidget):
         folder_text = prompt.folder(title=self.textbox.text(), directory=self.textbox.text())
         self.textbox.setText(folder_text)
 
-    def mass_drive_selected(self):
-        self.textbox.setText(r'I:\MSL\Private\Mass')
-
-    def hdrive_selected(self):
-        self.textbox.setText(r'H:')
-
-    def sampledata_selected(self):
-        self.textbox.setText(r'I:\MSL\Private\Mass\transfer\Balance Software\Sample Data')
-
-
-class FileSelect(QtWidgets.QWidget):
-
-    def __init__(self, default, icon):
-        super(FileSelect, self).__init__()
-
-        self.textbox = QtWidgets.QLineEdit(default)
-        self.button = Button(icon=icon, left_click=self.display_file)
-        self.button.add_menu_item(text='Mass drive', triggered=self.mass_drive_selected)
-        self.button.add_menu_item(text='H: drive', triggered=self.hdrive_selected)
-        self.button.add_menu_item(text='Sample data', triggered=self.sampledata_selected)
-        #self.button.add_menu_item('open browser', shortcut='CTRL+O')
-
-        hbox = QtWidgets.QHBoxLayout()
-        hbox.addWidget(self.textbox)
-        hbox.addWidget(self.button)
-        self.setLayout(hbox)
-
     def display_file(self):
         folder_text = prompt.filename(title=self.textbox.text(), directory=self.textbox.text())
         self.textbox.setText(folder_text)
 
     def mass_drive_selected(self):
-        self.textbox.setText(r'I:\MSL\Private\Mass')
+        self.textbox.setText(mass_folder)
 
     def hdrive_selected(self):
-        self.textbox.setText(r'H:')
+        self.textbox.setText(H_drive)
 
     def sampledata_selected(self):
-        self.textbox.setText(r'I:\MSL\Private\Mass\transfer\Balance Software\Sample Data')
+        self.textbox.setText(sample_data_folder)
