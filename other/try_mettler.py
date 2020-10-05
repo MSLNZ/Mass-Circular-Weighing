a903dc6b406f3fb8173330c32212004cede292b3
r.hawke <rebecca.hawke@measurement.govt.nz>
2020-10-05 15:15:40 +1300

    Added helper scripts for testing balance communication is working.
    The first few lines of these scripts can be run in the console to load the relevant classes, then further commands can be sent to the balance using the 'bal' object.

---
+++ other/try_mettler.py
@@ -0,0 +1,49 @@
+from mass_circular_weighing.configuration import Configuration
+from mass_circular_weighing.equip.mettler import MettlerToledo
+
+
+cfg = r'C:\Users\r.hawke\PycharmProjects\Mass-Circular-Weighing\config.xml'
+
+app = Configuration(cfg)
+
+bal, mode = app.get_bal_instance('K_C')
+
+print(bal.unit)
+print(bal.get_serial())
+
+
+
+zeroes = []
+masses = []
+bal.load_bal('None', 1)
+bal.zero_bal()
+z = bal.get_mass_stable('')
+zeroes.append(z)
+bal.unload_bal('None', 1)
+
+for i in range(10):
+    bal.load_bal('Mass', 1)
+    m = bal.get_mass_stable(str(i))
+    masses.append(m)
+    bal.unload_bal('Mass', 1)
+    bal.load_bal('None', 1)
+    z = bal.get_mass_stable('')
+    zeroes.append(z)
+    bal.unload_bal('None', 1)
+#
+
+# print(bal.unit)
+for m in masses:
+    print(m)
+for z in zeroes:
+    print(z)
+
+
+bal.connection.disconnect()
+
+
+
+# bal.scale_adjust()
+'''
+bal.tare_bal()
+'''
