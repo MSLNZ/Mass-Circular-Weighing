d9a4ba32cd59ec9ed4428f386e7710e73f371c00
r.hawke <rebecca.hawke@measurement.govt.nz>
2020-11-25 12:05:41 +1300

    Updated try_mettler.py: use for repeatability checks

--- other/try_mettler.py
+++ other/try_mettler.py
@@ -1,3 +1,9 @@
+"""
+Test communications with a Mettler Toledo balance by performing a repeatability check
+"""
+
+from time import sleep
+
 from mass_circular_weighing.configuration import Configuration
 from mass_circular_weighing.equip.mettler import MettlerToledo
 
@@ -9,6 +15,7 @@ app = Configuration(cfg)
 bal, mode = app.get_bal_instance('K_C')
 
 print(bal.unit)
+print(bal.mode)
 print(bal.get_serial())
 
 
@@ -17,6 +24,7 @@ zeroes = []
 masses = []
 bal.load_bal('None', 1)
 bal.zero_bal()
+sleep(3)
 z = bal.get_mass_stable('')
 zeroes.append(z)
 bal.unload_bal('None', 1)
@@ -30,7 +38,7 @@ for i in range(10):
     z = bal.get_mass_stable('')
     zeroes.append(z)
     bal.unload_bal('None', 1)
-#
+
 
 # print(bal.unit)
 for m in masses:
