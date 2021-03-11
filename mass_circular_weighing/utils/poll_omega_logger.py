506d445bc208bebe14426a884c6266befc4a17ba
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2021-03-11 19:40:16 +1300

    Swapped webapp in place of labenviron

--- mass_circular_weighing/utils/poll_omega_logger.py
+++ mass_circular_weighing/utils/poll_omega_logger.py
@@ -5,7 +5,7 @@ Note that calibrated values are returned by default.
 import logging
 
 from mass_circular_weighing.constants import IN_DEGREES_C
-from mass_circular_weighing.equip import LabEnviron64
+from mass_circular_weighing.equip import get_t_rh_now
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -19,9 +19,7 @@ def poll_omega_logger(logger):
        one of 'mass 1', 'mass 2', 'temperature 1'
 
     """
-    dll = LabEnviron64()
-
     for sensor in [1, 2]:
         print('Collecting current ambient conditions from {}, sensor {}...'.format(logger, sensor))
-        date_start, t_start, rh_start = dll.get_t_rh_now(logger, sensor)
+        date_start, t_start, rh_start = get_t_rh_now(logger, sensor=sensor)
         print("Temperature: {:.2f}{}, Relative humidity: {:.2f} %".format(t_start, IN_DEGREES_C, rh_start))
