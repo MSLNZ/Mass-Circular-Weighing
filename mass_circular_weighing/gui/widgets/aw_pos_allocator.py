1c455b342628bf8beee3bb280074a8ffa476d69f
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2021-04-21 13:46:26 +1200

    Revised position allocator widget using inbuilt checkboxes in list
    Now only two list columns rather than three so 'should' display better.

--- mass_circular_weighing/gui/widgets/aw_pos_allocator.py
+++ mass_circular_weighing/gui/widgets/aw_pos_allocator.py
@@ -2,9 +2,7 @@
 The Allocator window allows the operator to specify positions for each weight group, which positions to centre (if any),
 and the position to be used for balance self-calibration (if desired).
 """
-import numpy as np
-
-from msl.qt import QtWidgets, Button
+from msl.qt import Qt, QtWidgets, Button
 
 
 class AllocatorDialog(QtWidgets.QDialog):
@@ -29,36 +27,26 @@ class AllocatorDialog(QtWidgets.QDialog):
         self.wtgrps = wtgrps
         self.positions = []
         self.pos_to_centre = []
+        self.centrings = 0
         self.cal_pos = 1
         self.want_adjust = True
 
         self.pos_list = QtWidgets.QListWidget()
         self.pos_list.addItems([f'Position {i+1}' for i in range(num_pos)])
-        self.pos_list.setMaximumWidth(self.pos_list.sizeHintForColumn(0) + 6)
-        max_list_height = 1.25 * num_pos * self.pos_list.sizeHintForRow(0)
-        self.pos_list.setMaximumHeight(max_list_height)
+        self.pos_list.setMaximumWidth(int(1.5*self.pos_list.sizeHintForColumn(0)))
+        self.pos_list.setMinimumWidth(self.pos_list.sizeHintForColumn(0))
 
         self.wtgrp_list = QtWidgets.QListWidget()
-        self.wtgrp_list.addItems(self.wtgrps)
-        while self.wtgrp_list.count() < num_pos:
-            self.wtgrp_list.addItem('empty')
+        for wtgrp in self.wtgrps + ['empty'] * (num_pos - len(self.wtgrps)):
+                item = QtWidgets.QListWidgetItem(wtgrp)
+                item.setCheckState(Qt.Unchecked)
+                self.wtgrp_list.addItem(item)
         self.wtgrp_list.setDragDropMode(QtWidgets.QListWidget.InternalMove)
-        self.wtgrp_list.setMaximumHeight(max_list_height)
-
-        self.centre_list = QtWidgets.QListWidget()
-        while self.centre_list.count() < num_pos:
-            item = QtWidgets.QListWidgetItem(self.centre_list)
-            ch = QtWidgets.QCheckBox()
-            self.centre_list.setItemWidget(item, ch)
-        self.centre_list.setMaximumWidth(self.centre_list.sizeHintForColumn(0) + 5)
-        self.centre_list.setMaximumHeight(max_list_height)
 
         lists = QtWidgets.QWidget()
         hbox = QtWidgets.QHBoxLayout()
         hbox.addWidget(self.pos_list)
-        hbox.addWidget(self.centre_list)
         hbox.addWidget(self.wtgrp_list)
-
         lists.setLayout(hbox)
 
         shuffle = Button(text='Shuffle all weight groups down one position', left_click=self.roll)
@@ -92,34 +80,25 @@ class AllocatorDialog(QtWidgets.QDialog):
 
         self.setLayout(vbox)
 
-    def get_all_items(self):
-        return [self.wtgrp_list.item(i).text() for i in range(self.wtgrp_list.count())]
-
-    def get_centrings(self,):
-        pos_to_centre = []
-        for i in range(self.num_pos):
-            if self.centre_list.itemWidget(self.centre_list.item(i)).checkState():
-                pos_str = self.pos_list.item(i).text().strip("Position ")
-                pos_to_centre.append(int(pos_str))
-
-        return pos_to_centre
-
     def roll(self):
-        loading = self.get_all_items()
-        self.wtgrp_list.clear()
-        self.wtgrp_list.addItems(np.roll(loading, 1))
+        lastItem = self.wtgrp_list.takeItem(self.num_pos-1)
+        self.wtgrp_list.insertItem(0, lastItem)
 
     def accept_loading_order(self,):
-        self.centrings = int(self.num_centrings.text())
+        loading = []
+        for i in range(self.num_pos):
+            loading.append(self.wtgrp_list.item(i).text())
+            if self.wtgrp_list.item(i).checkState():
+                pos_str = self.pos_list.item(i).text().strip("Position ")
+                self.pos_to_centre.append(int(pos_str))
 
-        self.pos_to_centre = self.get_centrings()
+        for grp in self.wtgrps:
+            self.positions.append(loading.index(grp)+1)
+
+        self.centrings = int(self.num_centrings.text())
 
         self.cal_pos = int(self.cal_pos_box.text())
         self.want_adjust = True if self.adjust_ch.checkState() else False
-
-        loading = self.get_all_items()
-        for grp in self.wtgrps:
-            self.positions.append(loading.index(grp)+1)
 
         self.close()
 
