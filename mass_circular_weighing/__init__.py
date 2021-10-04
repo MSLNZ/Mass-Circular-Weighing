b3b54c57cb0e9315bf92164af18d6562830959ad
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2021-10-04 18:21:00 +1300

    bump v1.0.2 to v1.0.3.dev0

--- mass_circular_weighing/__init__.py
+++ mass_circular_weighing/__init__.py
@@ -2,7 +2,7 @@ from .log import log
 
 __author__ = 'Measurement Standards Laboratory of New Zealand'
 __copyright__ = '\xa9 2021, ' + __author__
-__version__ = '1.0.2'
+__version__ = '1.0.3.dev0'
 
 from .gui.gui import show_gui
 from .utils.poll_omega_logger import poll_omega_logger
