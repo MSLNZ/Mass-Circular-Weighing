b921ffcfadb94ca67e4abcb8d138e35c6365a8b5
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2022-01-28 14:46:29 +1300

    specify install of PyQt5 and submodules
    in order to build working .exe

--- standalone/make.bat
+++ standalone/make.bat
@@ -1,2 +1,3 @@
 @ECHO off
-pyinstaller --clean --noconfirm --additional-hooks-dir . mass_circular_weighing_standalone.py
+python -c "import sys; print(sys.executable)"
+python -m PyInstaller --clean --noconfirm --additional-hooks-dir . mass_circular_weighing_standalone.py --hidden-import PyQt5 --hidden-import PyQt5.QtGui --hidden-import PyQt5.QtWidgets --hidden-import PyQt5.QtSvg --hidden-import QtCore.Qt
