fefd47e8c65fa542d23650ac3a122c21889b4933
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2023-12-05 10:45:28 +1300

    modified LST file exporter
     for buildup so that it doesn't need manual entry of the scheme entry or the save filename
    
     using openpyxl
    
     not properly integrated into the weighing program yet

--- mass_circular_weighing/routines/convert_json2LST.py
+++ mass_circular_weighing/routines/convert_json2LST.py
@@ -11,31 +11,32 @@ from msl.equipment import Config
 from mass_circular_weighing.routine_classes.circ_weigh_class import CircWeigh
 from mass_circular_weighing.constants import IN_DEGREES_C
 
-cfg = Config(r"C:\MCW_Config\local_config.xml")
 
-folder = r'I:\MSL\Private\Mass\Commercial Calibrations\2023\AX1006PostService'  # folder of data
-json_file = r'I:\MSL\Private\Mass\Commercial Calibrations\2023\AX1006PostService\backups\MassStds_1000-28-11-23.json'
+def to_LST(jsonroot, save_folder, cfg=None):
+    lst_filename = ""
+    for grp in jsonroot['Circular Weighings'].groups():
+        ambient_data = [""]
+        to_mg = 1
+        bal_alias = None
+        nom = 0
 
-jsonroot = read(json_file)
-print(jsonroot)
-for grp in jsonroot['Circular Weighings'].groups():
-    se = grp.name.split("/")[-1]
-    cw_class = CircWeigh(se)
+        se = grp.name.split("/")[-1]
+        cw_class = CircWeigh(se)
 
-    wb = Workbook()
-    sheet1 = wb.active
-    sheet1.title = "LST file"
-    header = []
-    for pos, grp in enumerate(cw_class.wtgrps):
-        header.append(grp + '(#' + str(pos+1) + ")")
-    sheet1.append(header)
-    sheet1.append(["=F4", cw_class.num_cycles])
-    sheet1.append([])
+        wb = Workbook()
+        sheet1 = wb.active
+        sheet1.title = "LST file"
+        header = []
+        for pos, grp in enumerate(cw_class.wtgrps):
+            header.append(grp + '(#' + str(pos+1) + ")")
+        sheet1.append(header)
+        sheet1.append(["=F4", cw_class.num_cycles])
+        sheet1.append([])
 
-    for weighdata in jsonroot.datasets():
+        for weighdata in jsonroot.datasets():
             if 'measurement' in weighdata.name:
+                nom = weighdata.metadata.get("Nominal mass (g)")
                 if weighdata.metadata.get("Weighing complete"):
-                    run_no = weighdata.name.split('_')[-1]
                     if weighdata.metadata.get("Unit") == "g":
                         to_mg = 1000
                     elif weighdata.metadata.get("Unit") == "mg":
@@ -49,7 +50,7 @@ for grp in jsonroot['Circular Weighings'].groups():
                             data_row.append(cyc_val[i][1]*to_mg)
                         if cycle == 0:
                             try:
-                                # TODO calculate actual mean and save to metadata as part of run_circ_weigh
+                                # could calculate actual mean and save to metadata as part of run_circ_weigh?
                                 temps = weighdata.metadata.get("T" + IN_DEGREES_C).split(" to ")
                                 mean_temp = (float(temps[0]) + float(temps[1])) / 2
                                 p = weighdata.metadata.get("Pressure (hPa)").split(" to ")
@@ -64,22 +65,43 @@ for grp in jsonroot['Circular Weighings'].groups():
                                     mean_P,
                                     mean_rhs
                                 ]
+                                print(ambient_data)
                                 data_row += ambient_data
                                 ambient_data[2] = float(temps[1])
                             except AttributeError:
                                 pass
                         bal_alias = weighdata.metadata.get("Balance")
-                        db = cfg.database()
-                        bal_serial = db.equipment[bal_alias].serial
 
                         sheet1.append(data_row)
 
-    last_row = ['', '', '', '']
-    last_row += ambient_data
-    sheet1.append(last_row)
+        last_row = ['', '', '', '']
+        last_row += ambient_data
+        sheet1.append(last_row)
 
-    sheet1["F2"] = bal_serial
+        try:
+            bal_serial = cfg.db.equipment[bal_alias].serial
+        except AttributeError:
+            cfg = Config(r"C:\MCW_Config\local_config.xml")
+            db = cfg.database()
+            bal_serial = db.equipment[bal_alias].serial
+        sheet1["F2"] = bal_serial
 
-    last_date = ambient_data[1].split()[0]
+        print(ambient_data)
+        last_date = ambient_data[1].split()[0]
 
-    wb.save(os.path.join(folder, last_date + "_" + se + "_LST.xlsx"))
+        lst_filename = os.path.join(save_folder, last_date + "_" +str(nom) + "_" + se + "_LST.xlsx")
+        wb.save(lst_filename)
+
+    return lst_filename
+
+
+if __name__ == "__main__":
+    folder = r'I:\MSL\Private\Mass\Recal_2020\kgs\PostBIPMAX1006files'  # folder of data
+
+    json_file = r'I:\MSL\Private\Mass\Recal_2020\kgs\PostBIPMAX1006files\MassStds_1000.json'
+    jsonroot = read(json_file)
+    print(jsonroot)
+
+    cfg=Config(r"C:\MCW_Config\local_config.xml")
+
+    to_LST(jsonroot=jsonroot, save_folder=folder, cfg=cfg)
