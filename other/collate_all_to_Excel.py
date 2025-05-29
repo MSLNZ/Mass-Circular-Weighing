d6136cc9d9017bdbb51ad81f26754853edb90c54
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2025-05-29 15:41:08 +1200

    collate acceptance criteria
    script updated to gather statistics for 100-500 g weighings on XPE505C

--- other/collate_all_to_Excel.py
+++ other/collate_all_to_Excel.py
@@ -4,44 +4,47 @@ A script to collate all weighing data from json files to determine acceptance cr
 
 import os
 from openpyxl import Workbook
+import numpy as np
+from datetime import datetime
 
 from msl.io import read
 
 # from mass_circular_weighing.constants import MU_STR, SUFFIX
 
-folder = r'M:\Commercial Calibrations\2025\UMX-5\Linearity' # folder of data
+folder = r'M:\Commercial Calibrations'  #\2025\0011_Glycosyn'  #\UMX-5\Linearity'  # folder of data
 
 summary = Workbook()                            # create new Excel doc
+summary.iso_dates = True                        # store dates and times in the ISO8601 format
 runs = summary.active                           # get active sheet
 runs.title = "Individual Runs"                  # set title
 runs.append([
-    "File", "Mmt Date", "Nominal (g)", "Run #", 'no drift', 'linear drift', 'quadratic drift', 'cubic drift',
+    "File", "Mmt Date", "Bal", "Nominal (g)", "Scheme Entry", "Run #", "no drift", "linear drift", 'quadratic drift', 'cubic drift',
 ])
 collated = summary.create_sheet("Collated")     # make new sheet for collated runs
 collated.append([
-    "File", "Mmt Date", "Nominal (g)", "Scheme entry", "Run #", "+ weight group", "- weight group",
+    "File", "Mmt Date", "Bal", "Nominal (g)", "Scheme entry", "Run #", "+ weight group", "- weight group",
     "mass difference (g)", "residual (µg)", "balance uncertainty (µg)", "Acceptance met?",
 ])
 
 for flist in os.walk(folder):                   # traverse all folders and subfolders
-    if 'backups' not in flist[0]:               # unless they are called 'backups'
+    if 'executable_archives' in flist[0]:
+        print('executables')
+    elif 'ackup' not in flist[0]:  # unless they are called 'backups' or 'Backups'
         for fname in flist[2]:                  # iterate through the files
             if '.json' in fname and 'finalmasscalc' not in fname:   # select only json files from weighings
                 file_to_read = os.path.join(flist[0], fname)
-                # print(f'Reading {file_to_read}')
+                print(f'Reading {file_to_read}')
                 jsonroot = read(file_to_read)
-
                 nom = None
                 date = None
                 for ds in jsonroot.datasets():
                     if 'measurement_run_1' in ds.name:
                         nom = ds.metadata['Nominal mass (g)']
-                if 0 < nom < 5.5:
-                    print(nom, file_to_read.strip(folder))
+                if 110 < nom < 550:
+                    # print(nom, file_to_read.strip(folder))
                     for ds in jsonroot.datasets():
                         if 'measurement_run_1' in ds.name:
                             bal = ds.metadata["Balance"]
-                            # assert bal == "XPE505C"             # just in case any were on the old Sartorius
                         if 'analysis' in ds.name:
                             mmt = ds.name.replace('analysis', 'measurement')
                             mmt_ds = jsonroot[mmt]
@@ -49,7 +52,9 @@ for flist in os.walk(folder):                   # traverse all folders and subfo
                             run_data = [
                                 file_to_read.strip(folder),
                                 date,
+                                mmt_ds.metadata["Balance"],
                                 nom,
+                                ds.name.split("/")[2],  # scheme entry
                                 ds.name.split("_")[-1]
                             ]
                             res_dict = eval(ds.metadata["Residual std devs"])
@@ -61,8 +66,9 @@ for flist in os.walk(folder):                   # traverse all folders and subfo
                             mmt = ds.name.replace('Collated', 'measurement_run_1')
                             mmt_ds = jsonroot[mmt]
                             date = mmt_ds.metadata["Mmt Timestamp"]
+                            bal = mmt_ds.metadata["Balance"]
                             for r in ds.data:
-                                row = [file_to_read.strip(folder), date]
+                                row = [file_to_read.strip(folder), date, bal]
                                 for i in r:
                                     row.append(i)
                                 collated.append(row)
@@ -71,9 +77,12 @@ for flist in os.walk(folder):                   # traverse all folders and subfo
 runs.insert_cols(3, amount=1)
 runs['C1'] = "Mmt Time"
 for i in range(2, runs.max_row+1):
-    date_time = runs['B'+str(i)].value.split()
-    runs['B' + str(i)] = date_time[0].replace("-", "/")
-    runs['C' + str(i)] = date_time[1]
+    try:
+        date_time = datetime.strptime(runs['B'+str(i)].value, '%d-%m-%Y %H:%M:%S')
+    except ValueError:
+        date_time = datetime.strptime(runs['B' + str(i)].value, '%d-%m-%Y %H:%M')
+    runs['B' + str(i)] = date_time.date()
+    runs['C' + str(i)] = date_time.time()
 
 collated.insert_cols(3, amount=1)
 collated['C1'] = "Mmt Time"
