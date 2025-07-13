81e4b398639ef33a10eb533e2f54284101325d05
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2025-07-14 11:28:13 +1200

    air density calculator
    script used to calculate mean air density using json_circweigh_utils add_air_densities (which uses lists of all P, T, RH during the weighings)

---
+++ other/air_density_calculator.py
@@ -0,0 +1,25 @@
+
+from msl.io import JSONWriter, read
+from msl.equipment import Config
+
+from mass_circular_weighing.routines.json_circweigh_utils import add_air_densities
+from mass_circular_weighing.routines.analyse_circ_weigh import analyse_weighing_true_mass
+
+
+cfg = Config(r"C:\MCW_Config\local_config.xml")
+folder = r'M:\Recal_2025_buildup\2025_06_09 June 9 Buoyancy'  # folder of data
+url = r'M:\Recal_2025_buildup\2025_06_09 June 9 Buoyancy\MSL_100000.json'
+
+json__root = read(url)
+json__root.read_only = False
+print(json__root)
+
+new_root = add_air_densities(root=json__root)
+
+# new_root = analyse_weighing_true_mass(
+#         cfg, json__root, url, se="20KA 10KAM+10KBM 20KB 20KC", run_id='run_1', bal_mode='mw')
+# new_url = r'M:\Recal_2025_buildup\2025_06_09 June 9 Buoyancy\MSL_50000_tm.json'
+
+root = JSONWriter()
+root.set_root(new_root)
+root.save(file=url, mode='w', encoding='utf-8', ensure_ascii=False)
