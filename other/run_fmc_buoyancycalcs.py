9f89f130f7f6a5491010634731001d4748c8bf71
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2025-07-14 11:19:07 +1200

    FMC with buoyancy
    Run final mass calculation using collated data from json files, with ability to customise FMC
    
    Note that the summary file generation is very basic at this stage

---
+++ other/run_fmc_buoyancycalcs.py
@@ -0,0 +1,123 @@
+# Run final mass calculation using collated data from json files, with ability to customise FMC
+
+import sys
+import os
+
+from msl.qt import application, excepthook
+sys.excepthook = excepthook
+gui = application()
+
+import numpy as np
+from openpyxl.styles import Font, Alignment
+
+from msl.io import read, read_table
+
+from mass_circular_weighing.configuration import Configuration
+from mass_circular_weighing.constants import MU_STR
+
+from mass_circular_weighing.gui.widgets.scheme_table import SchemeTable
+from mass_circular_weighing.routines.collate_data import collate_all_weighings
+
+from mass_circular_weighing.routine_classes.final_mass_calc_class import FinalMassCalc
+from mass_circular_weighing.gui.threads.masscalc_popup import filter_mass_set
+
+from mass_circular_weighing.routine_classes.results_summary_Excel import ExcelSummaryWorkbook
+
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+admin = r"M:\Recal_2025_buildup\2025_06_09 June 9 Buoyancy\MSL_Admin.xlsx"
+
+# input_data is in json files
+
+cfg = Configuration(admin)
+print(cfg.correlations)
+print(cfg.ad_corr)
+cfg.init_ref_mass_sets()
+
+print(cfg.all_stds)
+print(cfg.all_client_wts)
+print(cfg.all_checks)
+
+# Load scheme
+schemetable = SchemeTable()
+schemetable.update_balance_list(cfg.bal_list)
+schemetable.load_scheme(cfg.scheme[0], cfg.scheme[1])
+# schemetable.show()
+# gui.exec()
+
+# Collate data, including air density information - use add_air_densities(root) if not already in root
+collated_data = collate_all_weighings(schemetable, cfg)[3:]  # first run was re-done
+collated_data = np.delete(collated_data, [3, 4, 5])
+"""We can change the balance uncertainty here if desired"""
+for i in range(24):
+    collated_data['balance uncertainty ('+MU_STR+'g)'][i] = 11000
+print(collated_data)
+
+input_data = np.empty(len(collated_data),
+                    dtype=[('+ weight group', object), ('- weight group', object),
+                           ('mass difference (g)', 'float64'), ('balance uncertainty (' + MU_STR + 'g)', 'float64')
+                           ],
+                    )
+
+for key in ['+ weight group', '- weight group', 'mass difference (g)', 'balance uncertainty (' + MU_STR + 'g)']:
+    input_data[key] = collated_data[key]
+
+client_wts = filter_mass_set(cfg.all_client_wts, collated_data)
+checks = None  # filter_mass_set(cfg.all_checks, collated)  # need to change back to filter_mass_set for D1
+stds = filter_mass_set(cfg.all_stds, collated_data)
+
+fmc = FinalMassCalc(cfg.folder, cfg.client, client_wts, checks, stds, input_data, nbc=False, corr=cfg.correlations)
+
+
+# settings for analysis options
+fmc.BUOYANCY_CORR = True
+fmc.HEIGHT_CORR = False
+
+fmc.UNC_AIR_DENS = False  # here this is negligible compared to the weighing uncertainty
+fmc.UNC_VOL = False
+fmc.UNC_HEIGHT = False
+
+fmc.TRUE_MASS = False
+
+
+# do calculation
+fmc.import_mass_lists()
+fmc.parse_inputdata_to_matrices()
+
+fmc.apply_corrections_to_mass_differences(air_densities=collated_data['Mean air density (kg/m3)'])
+fmc.cal_psi_y(
+    unc_airdens=collated_data['Stdev air density (kg/m3)'],
+    air_densities=collated_data['Mean air density (kg/m3)'],
+    rv=None
+)
+
+fmc.do_least_squares()
+fmc.check_residuals()
+
+fmc.cal_rel_unc()
+fmc.make_summary_table()
+
+fmc.add_data_to_root()
+fmc.save_to_json_file()
+
+# Export to Excel summary file
+xl = ExcelSummaryWorkbook(cfg)
+xl.format_scheme_file()
+fmc_root = read(fmc.filesavepath)  # the metadata parsing breaks if trying to use fmc.finalmasscalc directly
+xl.add_mls(fmc_root)
+
+
+
+
+# add covariance matrix info to mls sheet
+mls = xl.wb["MLS Output Data"]
+mls.append([])  # Makes a new empty row
+mls.append(["Covariance matrix in g"])
+mls.append([" "]+fmc.all_wts['Weight ID'])
+for i, row in enumerate(fmc.covariances):
+    mls.append([fmc.all_wts['Weight ID'][i]]+list(row))
+
+xl_output_file = xl.save_workbook(cfg.folder, cfg.client)
+# set Excel output to read_only
+# os.chmod(xl_output_file, S_IREAD | S_IRGRP | S_IROTH)
